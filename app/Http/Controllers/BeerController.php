581668e60f0d472843d2c722b0be74552a090654
Robert Miner <robertminer1991@gmail.com>
2020-11-23 11:54:20 -0700

    Removed Comment

--- app/Http/Controllers/BeerController.php
+++ app/Http/Controllers/BeerController.php
@@ -8,7 +8,6 @@ use Illuminate\Http\Request;
 class BeerController extends Controller
 {
     public static function getBeerById($beerId) {
-        // needs to be find by or summin like that
         return Beer::where('id', $beerId)->first()->toJson();
     }
     public static function getAll() {
