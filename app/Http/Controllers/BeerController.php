93be3589cd1ba0a138c3bb8ab635a169c336177f
Robert Miner <robertminer1991@gmail.com>
2020-11-23 11:43:27 -0700

    Add Single Beer Endpoint

--- app/Http/Controllers/BeerController.php
+++ app/Http/Controllers/BeerController.php
@@ -7,8 +7,10 @@ use Illuminate\Http\Request;
 
 class BeerController extends Controller
 {
-    //
-
+    public static function getBeerById($beerId) {
+        // needs to be find by or summin like that
+        return Beer::where('id', $beerId)->first()->toJson();
+    }
     public static function getAll() {
         return Beer::all();
     }
