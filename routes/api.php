3e1e3c39d57968cf1dc8e203a604a1ac56d122d2
vagrant <vagrant@homestead>
2020-11-23 20:16:23 +0000

    Add POST brewery

--- routes/api.php
+++ routes/api.php
@@ -21,16 +21,31 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/brewery', function (Request $request) {
+/**
+ * Brewery routes
+ */
+Route::get('/brewery', function(Request $request) {
     return BreweryController::getAll($request->query('withBeers'));
 });
 
+Route::post('/brewery', function(Request $request) {
+    return BreweryController::create($request->all());
+});
+
+/**
+ * Retailer routes
+ */
 Route::get('/retailer', [RetailerController::class, 'getAll']);
 Route::get('/retailer/{retailerId}/menu', function($retailerId) {
     return MenuController::getMenuByRetailerId($retailerId);
 });
 
+/**
+ * Beer routes
+ */
 Route::get('/beer', [BeerController::class, 'getAll']);
 Route::get('/beer/{beerID}', function($beerID) {
     return BeerController::getBeerById($beerID);
 });
+
+
